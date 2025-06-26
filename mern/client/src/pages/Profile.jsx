d5ad4dd86735ba3f0c6fc2a6990d0e50ed4ab3a2
Lycoris21 <143074000+Lycoris21@users.noreply.github.com>
2025-06-26 17:52:45 +0800

    updated profile page so that users can see edit profile button if viewing their own profile and see like or dislike buttons if viewing not their own profile

--- mern/client/src/pages/Profile.jsx
+++ mern/client/src/pages/Profile.jsx
@@ -49,44 +49,51 @@ export default function Profile() {
                 <div className="relative h-60 bg-cover bg-center rounded-sm mb-6" style={{
                     backgroundImage: `url(${profileData.coverPhotoUrl})`,
                 }}>
-                    <Link
-                        to="/EditProfile"
-                        className="absolute bottom-4 right-4 border-[0.5px] border-[#1E275E] bg-white text-[#1E275E] py-2 px-4 rounded hover:bg-[#1E275E] hover:text-white hover:border-white"
-                    >
-                        Edit Profile
-                    </Link>
-
-                    <div className="absolute -bottom-17 right-16 flex space-x-2">
-                        <button className="bg-[#1E275E] text-white py-1 px-3 rounded hover:bg-[#4B548B] flex items-center cursor-pointer">
-                            {/* srry for the long ass svgs */}
-                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="mr-2">
-                                <path d="M23 10C23 9.46957 22.7893 8.96086 22.4142 8.58579C22.0391 8.21071 21.5304 8 21 8H14.68L15.64 3.43C15.66 3.33 15.67 3.22 15.67 3.11C15.67 2.7 15.5 2.32 15.23 2.05L14.17 1L7.59 7.58C7.22 7.95 7 8.45 7 9V19C7 19.5304 7.21071 20.0391 7.58579 20.4142C7.96086 20.7893 8.46957 21 9 21H18C18.83 21 19.54 20.5 19.84 19.78L22.86 12.73C22.95 12.5 23 12.26 23 12V10ZM1 21H5V9H1V21Z" fill="white" />
-                            </svg>
-                            Like
-                        </button>
-                        <button className="bg-[#1E275E] text-white py-1 px-3 rounded hover:bg-[#4B548B] flex items-center cursor-pointer">
-                            {/* srry for the long ass svgs */}
-                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="mr-2">
-                                <path d="M19 15H23V3H19M15 3H6C5.17 3 4.46 3.5 4.16 4.22L1.14 11.27C1.05 11.5 1 11.74 1 12V14C1 14.5304 1.21071 15.0391 1.58579 15.4142C1.96086 15.7893 2.46957 16 3 16H9.31L8.36 20.57C8.34 20.67 8.33 20.77 8.33 20.88C8.33 21.3 8.5 21.67 8.77 21.94L9.83 23L16.41 16.41C16.78 16.05 17 15.55 17 15V5C17 4.46957 16.7893 3.96086 16.4142 3.58579C16.0391 3.21071 15.5304 3 15 3Z" fill="white" />
-                            </svg>
-                            Dislike
-                        </button>
-                    </div>
                 </div>
 
                 {/* Profile Info */}
-                <div className="flex items-center mb-6">
-                    <img
-                        src={profileData.profilePictureUrl}
-                        alt={`${profileData.username}'s profile`}
-                        className="w-16 h-16 rounded-full mr-4"
-                    />
-                    <div>
-                        <h1 className="text-3xl font-bold">{profileData.username}</h1>
-                        <p className="text-gray-500">
-                            {profileData.likes} likes | {profileData.dislikes} dislikes
-                        </p>
-                        <p className="italic">{profileData.bio}</p>
+                <div className="flex items-center justify-between mb-6">
+                    <div className="flex items-center">
+                        <img
+                            src={profileData.profilePictureUrl}
+                            alt={`${profileData.username}'s profile`}
+                            className="w-16 h-16 rounded-full mr-4"
+                        />
+                        <div>
+                            <h1 className="text-3xl font-bold">{profileData.username}</h1>
+                            <p className="text-gray-500">
+                                {profileData.likes} likes | {profileData.dislikes} dislikes
+                            </p>
+                            <p className="italic">{profileData.bio}</p>
+                        </div>
+                    </div>
+                    <div className="flex space-x-2">
+                        {/* Show Edit Profile only for current user's own profile */}
+                        {user && profileData && user._id === profileData._id ? (
+                            <Link
+                                to="/EditProfile"
+                                className="border-[0.5px] border-[#1E275E] bg-white text-[#1E275E] py-1 px-4 rounded hover:bg-[#1E275E] hover:text-white hover:border-white">
+                                Edit Profile
+                            </Link>
+                        ) : (
+                            /* Show Like/Dislike for other users' profiles */
+                            <>
+                                <button className="bg-[#1E275E] text-white py-1 px-3 rounded hover:bg-[#4B548B] flex items-center cursor-pointer">
+                                    {/* srry for the long ass svgs */}
+                                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="mr-2">
+                                        <path d="M23 10C23 9.46957 22.7893 8.96086 22.4142 8.58579C22.0391 8.21071 21.5304 8 21 8H14.68L15.64 3.43C15.66 3.33 15.67 3.22 15.67 3.11C15.67 2.7 15.5 2.32 15.23 2.05L14.17 1L7.59 7.58C7.22 7.95 7 8.45 7 9V19C7 19.5304 7.21071 20.0391 7.58579 20.4142C7.96086 20.7893 8.46957 21 9 21H18C18.83 21 19.54 20.5 19.84 19.78L22.86 12.73C22.95 12.5 23 12.26 23 12V10ZM1 21H5V9H1V21Z" fill="white" />
+                                    </svg>
+                                    Like
+                                </button>
+                                <button className="bg-[#1E275E] text-white py-1 px-3 rounded hover:bg-[#4B548B] flex items-center cursor-pointer">
+                                    {/* srry for the long ass svgs */}
+                                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="mr-2">
+                                        <path d="M19 15H23V3H19M15 3H6C5.17 3 4.46 3.5 4.16 4.22L1.14 11.27C1.05 11.5 1 11.74 1 12V14C1 14.5304 1.21071 15.0391 1.58579 15.4142C1.96086 15.7893 2.46957 16 3 16H9.31L8.36 20.57C8.34 20.67 8.33 20.77 8.33 20.88C8.33 21.3 8.5 21.67 8.77 21.94L9.83 23L16.41 16.41C16.78 16.05 17 15.55 17 15V5C17 4.46957 16.7893 3.96086 16.4142 3.58579C16.0391 3.21071 15.5304 3 15 3Z" fill="white" />
+                                    </svg>
+                                    Dislike
+                                </button>
+                            </>
+                        )}
                     </div>
                 </div>
 
